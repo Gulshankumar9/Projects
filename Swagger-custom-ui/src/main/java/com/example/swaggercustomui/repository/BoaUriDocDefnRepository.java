e57d46fa861987c4ddc44586459cae4856e8f4b1
Gulshankumar9 <gulshan.gk64.gk@gmail.com>
2024-04-09 15:50:34 +0530

    reverting back to the prevous projects

--- Swagger-custom-ui/src/main/java/com/example/swaggercustomui/repository/BoaUriDocDefnRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.swaggercustomui.repository;
-
-import com.example.swaggercustomui.persistance.BoaUriDocDefn;
-import com.example.swaggercustomui.persistance.BoaUriDocDefnId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoaUriDocDefnRepository extends JpaRepository<BoaUriDocDefn, BoaUriDocDefnId> {
-}
